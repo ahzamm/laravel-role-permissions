3b38ed0472571b48db274ec5d585573c0c38cab5
ahzam <ahzamahmed6@gmail.com>
2024-02-27 14:28:08 +0500

    create login page

---
+++ resources/views/login.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/login.css') }}">
+
+</head>
+
+<body>
+    <form action="/admin/login" method="POST">
+        <h2><strong>Login</strong></h2>
+        <form action="/admin/login" method="POST">
+            @csrf
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+            <input type="submit" value="Login">
+        </form>
+</body>
+
+</html>
