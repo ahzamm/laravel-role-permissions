cf16bf34fbccfb9bf01106142ff7d1cb1a6eb4c8
ahzam <ahzamahmed6@gmail.com>
2024-02-27 15:24:27 +0500

    add dashboardpannel

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,28 @@
-<h1>Hello</h1>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/dashboard.css') }}">
+    
+</head>
+
+<body>
+    <header>
+        <div>
+            <h1>Admin Dashboard</h1>
+        </div>
+        <div>
+            <a href="/admin/manage-users">Manage User</a>
+            <a href="/admin/manage-roles">Manage Roles</a>
+        </div>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+        &copy; 2024 Admin Dashboard. All Rights Reserved.
+    </footer>
+</body>
+
+</html>
